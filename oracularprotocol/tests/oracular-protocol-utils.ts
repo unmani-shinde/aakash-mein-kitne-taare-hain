a64ca83533d24c437a0249d739b6fc7282df9309
Unmani Shinde <mailto.unmani@gmail.com>
2024-08-07 00:59:26 +0530

    Initialize subgraph

---
+++ oracularprotocol/tests/oracular-protocol-utils.ts
@@ -0,0 +1,177 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  ApprovalForAll,
+  BatchMetadataUpdate,
+  CookieMinted,
+  CookieSentToGossip,
+  MetadataUpdate,
+  Transfer
+} from "../generated/OracularProtocol/OracularProtocol"
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createBatchMetadataUpdateEvent(
+  _fromTokenId: BigInt,
+  _toTokenId: BigInt
+): BatchMetadataUpdate {
+  let batchMetadataUpdateEvent = changetype<BatchMetadataUpdate>(newMockEvent())
+
+  batchMetadataUpdateEvent.parameters = new Array()
+
+  batchMetadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_fromTokenId",
+      ethereum.Value.fromUnsignedBigInt(_fromTokenId)
+    )
+  )
+  batchMetadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_toTokenId",
+      ethereum.Value.fromUnsignedBigInt(_toTokenId)
+    )
+  )
+
+  return batchMetadataUpdateEvent
+}
+
+export function createCookieMintedEvent(
+  tokenOwner: Address,
+  tokenId: BigInt
+): CookieMinted {
+  let cookieMintedEvent = changetype<CookieMinted>(newMockEvent())
+
+  cookieMintedEvent.parameters = new Array()
+
+  cookieMintedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenOwner",
+      ethereum.Value.fromAddress(tokenOwner)
+    )
+  )
+  cookieMintedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return cookieMintedEvent
+}
+
+export function createCookieSentToGossipEvent(
+  tokenOwner: Address,
+  tokenId: BigInt,
+  gossipNetworkId: Address
+): CookieSentToGossip {
+  let cookieSentToGossipEvent = changetype<CookieSentToGossip>(newMockEvent())
+
+  cookieSentToGossipEvent.parameters = new Array()
+
+  cookieSentToGossipEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenOwner",
+      ethereum.Value.fromAddress(tokenOwner)
+    )
+  )
+  cookieSentToGossipEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  cookieSentToGossipEvent.parameters.push(
+    new ethereum.EventParam(
+      "gossipNetworkId",
+      ethereum.Value.fromAddress(gossipNetworkId)
+    )
+  )
+
+  return cookieSentToGossipEvent
+}
+
+export function createMetadataUpdateEvent(_tokenId: BigInt): MetadataUpdate {
+  let metadataUpdateEvent = changetype<MetadataUpdate>(newMockEvent())
+
+  metadataUpdateEvent.parameters = new Array()
+
+  metadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_tokenId",
+      ethereum.Value.fromUnsignedBigInt(_tokenId)
+    )
+  )
+
+  return metadataUpdateEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
